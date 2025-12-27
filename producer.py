0b2d1402a206bdf70684c94a100a26d336322ea8
Jehoi-ga-ada <sayajehoiada@gmail.com>
2025-12-27 11:51:53 +0700

    feat(producer): price stream handler to redis

---
+++ producer.py
@@ -0,0 +1,42 @@
+import os 
+import asyncio
+import logging
+from binance_sdk_spot.spot import Spot, SPOT_WS_STREAMS_PROD_URL, ConfigurationWebSocketStreams
+from redis.asyncio import Redis
+
+# Initialize Redis Client
+r = Redis(host=os.getenv("REDIS_HOST", "localhost"), port=6379, decode_responses=True)
+
+# Initialize Spot client
+configuration_ws_streams = ConfigurationWebSocketStreams(
+    stream_url=os.getenv("STREAM_URL", SPOT_WS_STREAMS_PROD_URL)
+)
+client = Spot(config_ws_streams=configuration_ws_streams)
+
+async def handle_message(msg):
+    if "p" in msg:
+        await r.xadd("market_stream", {"s": msg['s'], "p": msg['p']})
+        print(f"Captured {msg['s']}: {msg['p']}")
+
+async def trade():
+    stop_event = asyncio.Event()
+    connection = None
+    try:
+        connection = await client.websocket_streams.create_connection()
+
+        stream = await connection.trade(
+            symbol="btcusdt",
+        )
+        
+        stream.on("message", lambda data: asyncio.create_task(handle_message(data.to_dict())))
+
+        await stop_event.wait()
+        await stream.unsubscribe()
+    except Exception as e:
+        logging.error(f"trade() error: {e}")
+    finally:
+        if connection:
+            await connection.close_connection(close_session=True)
+
+if __name__ == "__main__":
+    asyncio.run(trade())
