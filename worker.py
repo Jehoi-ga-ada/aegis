15da705f00832689b14aaa3fcf2f094aec4f9848
Jehoi-ga-ada <sayajehoiada@gmail.com>
2025-12-28 18:09:10 +0700

    feat(worker): monitoring using prometheus for worker, tracking for total messages processed, and the btc price

--- worker.py
+++ worker.py
@@ -1,9 +1,16 @@
 import os 
 import asyncio
+from prometheus_client import start_http_server, Counter, Gauge
 from redis.asyncio import Redis
+
+MESSAGES_PROCESSED = Counter('aegis_worker_messages_total', 'Total prices processed')
+LATEST_BTC_PRICE = Gauge('aegis_btc_price', 'Current BTC price in worker')
 
 async def main():
     r = Redis(host=os.getenv("REDIS_HOST", "localhost"), port=6379, decode_responses=True)
+
+    start_http_server(8000)
+
     last_id = '$'
 
     ref_price = None
@@ -16,6 +23,8 @@ async def main():
         for _, messages in events:
             for msg_id, data in messages:
                 cur_price = float(data['p'])
+                MESSAGES_PROCESSED.inc()
+                LATEST_BTC_PRICE.set(float(data['p']))
                 
                 if ref_price is None: 
                     ref_price = cur_price
