e53d4619836307452f1cbb11f20ca515a862f042
Jehoi-ga-ada <sayajehoiada@gmail.com>
2025-12-29 23:00:15 +0700

    tests: remove unrelevant test

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -1,16 +1,6 @@
 from locust import HttpUser, task
 
 class AegisStressTest(HttpUser):
-    # This simulates a producer sending trade data to your system
-    @task
-    def simulate_trade_signal(self):
-        self.client.post("/ingest", json={
-            "symbol": "BTC/USD",
-            "price": 95000.00,
-            "volume": 0.1,
-            "signal": "BUY"
-        })
-
     @task(3) # This task happens 3x more often (checking status)
     def check_system_health(self):
         self.client.get("/health")
